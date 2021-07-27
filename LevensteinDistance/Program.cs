159594535d8c201f761888511912d092c4b30752
den4ik124 <den4ik124@gmail.com>
2021-07-27 18:52:43 +0300

    Начал писать метод

--- LevensteinDistance/Program.cs
+++ LevensteinDistance/Program.cs
@@ -10,6 +10,22 @@ namespace LevensteinDistance
     {
         private static void Main(string[] args)
         {
+            string stringA = "колокол";
+            string stringB = "молоко";
+
+            int distance = LevensteinDistance(stringA, stringB);
+        }
+
+        private static int LevensteinDistance(string horizontal, string vertical)
+        {
+            int[,] array = new int[vertical.Length + 1, horizontal.Length + 1];
+
+            for (int i = 0; i < vertical.Length; i++)
+            {
+                for (int j = 0; j < horizontal.Length; j++)
+                {
+                }
+            }
         }
     }
 }
